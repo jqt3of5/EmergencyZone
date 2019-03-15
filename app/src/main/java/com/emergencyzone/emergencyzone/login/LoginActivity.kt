950b333af515dc673037562ed25c48e4fe8dee04
John Todd <jqt3of5@gmail.com>
2019-03-14 22:50:22 -0600

    Fixing the login just a little more

--- app/src/main/java/com/emergencyzone/emergencyzone/login/LoginActivity.kt
+++ app/src/main/java/com/emergencyzone/emergencyzone/login/LoginActivity.kt
@@ -14,23 +14,37 @@ import android.widget.TextView
 import com.emergencyzone.emergencyzone.R
 import android.text.Spanned
 import android.text.method.LinkMovementMethod
+import android.widget.ImageView
 
 
 class LoginActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.login_activity)
 
+        var close = findViewById<ImageView>(R.id.cancel_login)
+        close.setOnClickListener {
+            finish()
+        }
+
+        var username = findViewById<EditText>(R.id.username_edittext).text
+        var password = findViewById<EditText>(R.id.password_edittext).text
+
         var button = findViewById<Button>(R.id.login_button)
-        var username = findViewById<EditText>(R.id.username_edittext)
-        var password = findViewById<EditText>(R.id.password_edittext)
+        button.setOnClickListener {
+
+        }
 
         var forgotPasswordLink = findViewById<TextView>(R.id.forgot_password_link)
-        var source = "<a href='example.com'>Forgot Password</a>"
-        forgotPasswordLink.text = fromHtml(source)
+        var link = "<a href='https://www.emergencyzone.com/login_sendpass.asp'>Forgot Password</a>"
+        forgotPasswordLink.text = fromHtml(link)
         forgotPasswordLink.movementMethod = LinkMovementMethod.getInstance()
+
+        var dontHaveAccount = findViewById<TextView>(R.id.dont_have_account_link)
+        link = "<a href='https://www.emergencyzone.com/AccountSettings.asp?AddNewCustomer=Y&ReturnTo='>Don't have an account?</a>"
+        dontHaveAccount.text = fromHtml(link)
+        dontHaveAccount.movementMethod = LinkMovementMethod.getInstance()
     }
 
     fun fromHtml(source: String): Spanned {
