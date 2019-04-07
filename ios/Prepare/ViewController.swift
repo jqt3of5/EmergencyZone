e028e63944d9b3efc30bea120c4004821369bd27
John Todd <johntodd@Kiras-Air.hsd1.ut.comcast.net>
2019-04-07 17:54:31 -0600

    Adding ios project

---
+++ ios/Prepare/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prepare
+//
+//  Created by John Todd on 4/7/19.
+//  Copyright © 2019 John Todd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios/PrepareTests/PrepareTests.swift
@@ -0,0 +1,34 @@
+//
+//  PrepareTests.swift
+//  PrepareTests
+//
+//  Created by John Todd on 4/7/19.
+//  Copyright © 2019 John Todd. All rights reserved.
+//
+
+import XCTest
+@testable import Prepare
+
+class PrepareTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
